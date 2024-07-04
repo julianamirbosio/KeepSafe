fb25c40e943789170b009f23a3770a41b042faf5
Juliana <128743881+julianamirbosio@users.noreply.github.com>
2024-07-03 21:22:11 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from janela_logcad import KeepSafeLogCad
+
+if __name__ == "__main__":
+    print("Iniciando a aplicação")
+    app = KeepSafeLogCad()
+    app.run()
+    print("Aplicação encerrada")
